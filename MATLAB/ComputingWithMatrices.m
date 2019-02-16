5b37d1bb1357ffc6f9e8346a945a69220aabe19a
Ananya Pandey <anany.boss@gmail.com>
2019-02-16 16:01:35 +0530

    Computing with matrix

---
+++ MATLAB/ComputingWithMatrices.m
@@ -0,0 +1,28 @@
+% element wise multiplication of matrices
+
+newmat = MAt1 .* Mat2 
+
+% each element is multiplied to correspending element of other matrix.
+% use DOT for the element wise operation
+
+abs(matrix)
+log(mat1)
+exp(mat1)
+abs(mat1)
+
+max(mat1) % returns the maximum value of the matrix1
+[val,index] = max(mat1)  % returns index also 
+
+mat1 < 3 % returns all lelements as 0 or 1 which are less than 1 
+find(mat1<3)
+
+floor(mat1)
+ceil(mat1)
+
+% floor and ceiling function 
+
+max(max(mat1)) % returns the exact value that ie maximum or max(A(:))
+
+% WE can flip the matrix by flipup method
+
+flipud(eye(9))

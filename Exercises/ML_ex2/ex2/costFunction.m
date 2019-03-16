31f4bf7e966fd87436f6a01d405e6483c2d5faf0
Ananya Pandey <anany.boss@gmail.com>
2019-03-16 16:12:39 +0530

    neural Network Backpropogation

--- Exercises/ML_ex2/ex2/costFunction.m
+++ Exercises/ML_ex2/ex2/costFunction.m
@@ -19,6 +19,7 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+J = (-1 / m) .* sum(y.*log(sigmoid(X * theta)) + (1 - y).*log(1 - sigmoid(X * theta)));
 
 
 
--- Exercises/ML_ex2/ex2/plotData.m
+++ Exercises/ML_ex2/ex2/plotData.m
@@ -15,8 +15,8 @@ figure; hold on;
 pos = find(y==1);
 neg = find(y==0);
 
-plot(X(neg,1),X(neg,2), 'k+', 'Linewidth', 2, 'MarkerSize', 7, 'MarkerFaceColor', 'b');
-plot(X(pos,1),X(pos,2), 'ko', 'Linewidth', 2, 'MarkerSize', 7, 'MarkerFaceColor', 'y');
+plot(X(neg,1),X(neg,2), 'ko', 'Linewidth', 2, 'MarkerSize', 7, 'MarkerFaceColor', 'b');
+plot(X(pos,1),X(pos,2), 'k+', 'Linewidth', 2, 'MarkerSize', 7, 'MarkerFaceColor', 'y');
 
 
 
